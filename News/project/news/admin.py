e7fdcdff8adb4dd96a2ea9acf949dae3fa65739d
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2022-09-14 18:16:52 +0300

    Add files via upload

--- News/project/news/admin.py
+++ News/project/news/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'post_text', 'date_and_time_of_creation_post']
+    list_filter = ['date_and_time_of_creation_post', 'title']
+    search_fields = ['title']
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
